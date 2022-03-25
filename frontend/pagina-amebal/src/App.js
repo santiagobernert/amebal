2358ecb318fd4758f0c635dee2f79d8ea8a61f07
santiagobernert <s.bernert@alumno.etec.um.edu.ar>
2022-03-25 12:02:59 -0300

    25/3

---
+++ frontend/pagina-amebal/src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { Navbar } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="dark" variant="dark" className="center">
+        <Navbar.Brand>
+          <img src="https://placedog.net/50/50" alt="" />
+        </Navbar.Brand>
+        <Navbar.Brand>
+          <img src="https://placedog.net/50/50" alt="" />
+        </Navbar.Brand>
+        <Navbar.Brand>
+          <img src="https://placedog.net/50/50" alt="" />
+        </Navbar.Brand>
+        <Navbar.Brand>
+          <img src="https://placedog.net/50/50" alt="" />
+        </Navbar.Brand>
+        <Navbar.Brand>
+          <img src="https://placedog.net/50/50" alt="" />
+        </Navbar.Brand>
+        <Navbar.Brand>
+          <img src="https://placedog.net/50/50" alt="" />
+        </Navbar.Brand>
+        <Navbar.Brand>
+          <img src="https://placedog.net/50/50" alt="" />
+        </Navbar.Brand>
+        <Navbar.Brand>
+          <img src="https://placedog.net/50/50" alt="" />
+        </Navbar.Brand>
+        <Navbar.Brand>
+          <img src="https://placedog.net/50/50" alt="" />
+        </Navbar.Brand>
+        <Navbar.Brand>
+          <img src="https://placedog.net/50/50" alt="" />
+        </Navbar.Brand>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
