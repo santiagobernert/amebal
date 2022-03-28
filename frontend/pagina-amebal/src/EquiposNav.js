294bca7ea1dae0592df0625e589829d3b2bdb2bd
santiagobernert <s.bernert@alumno.etec.um.edu.ar>
2022-03-27 22:14:30 -0300

    27/3

--- frontend/pagina-amebal/src/EquiposNav.js
+++ frontend/pagina-amebal/src/EquiposNav.js
@@ -3,54 +3,70 @@ import "bootstrap/dist/css/bootstrap.css";
 import { Navbar } from "react-bootstrap";
 import styles from "./EquiposNav.module.css";
 
-export default function EquiposNav({link}) {
+import capital from "./imgs/equipos/capital.png";
+import etiec from "./imgs/equipos/etiec.png";
+import filippini from "./imgs/equipos/filippini.png";
+import gimnasia from "./imgs/equipos/gimnasia_y_esgrima.png";
+import tomba from "./imgs/equipos/godoy_cruz.png";
+import junin from "./imgs/equipos/junin.png";
+import lujan from "./imgs/equipos/lujan.png";
+import maipu from "./imgs/equipos/maipu.png";
+import palmira from "./imgs/equipos/palmira.png";
+import regatas from "./imgs/equipos/regatas.png";
+import rodeo from "./imgs/equipos/rodeo-del-medio.png";
+import russel from "./imgs/equipos/russel.png";
+import tunuyan from "./imgs/equipos/tunuyan.png";
+import tupungato from "./imgs/equipos/tupungato.png";
+import uncuyo from "./imgs/equipos/uncuyo.png";
+
+export default function EquiposNav() {
   return (
     <div>
       <Navbar justify-content="space-evenly" expand="lg" className={ styles.nav }>
         <Navbar.Brand>
-          <img src={link} alt="" />
+          <img className={styles.logo} src={tomba} alt="" />
         </Navbar.Brand>
         <Navbar.Brand>
-          <img src={link} alt="" />
+          <img className={styles.logo} src={regatas} alt="" />
         </Navbar.Brand>
         <Navbar.Brand>
-          <img src={link} alt="" />
+          <img className={styles.logo} src={etiec} alt="" />
         </Navbar.Brand>
         <Navbar.Brand>
-          <img src={link} alt="" />
+          <img className={styles.logo} src={gimnasia} alt="" />
         </Navbar.Brand>
         <Navbar.Brand>
-          <img src={link} alt="" />
+          <img className={styles.logo} src={junin} alt="" />
         </Navbar.Brand>
         <Navbar.Brand>
-          <img src={link} alt="" />
+          <img className={styles.logo} src={lujan} alt="" />
         </Navbar.Brand>
         <Navbar.Brand>
-          <img src={link} alt="" />
+          <img className={styles.logo} src={maipu} alt="" />
         </Navbar.Brand>
         <Navbar.Brand>
-          <img src={link} alt="" />
+          <img className={styles.logo} src={capital} alt="" />
         </Navbar.Brand>
         <Navbar.Brand>
-          <img src={link} alt="" />
+          <img className={styles.logo} src={palmira} alt="" />
         </Navbar.Brand>
         <Navbar.Brand>
-          <img src={link} alt="" />
+          <img className={styles.logo} src={tunuyan} alt="" />
         </Navbar.Brand>
         <Navbar.Brand>
-          <img src={link} alt="" />
+          <img className={styles.logo} src={tupungato} alt="" />
         </Navbar.Brand>
         <Navbar.Brand>
-          <img src={link} alt="" />
+          <img className={styles.logo} src={filippini} alt="" />
         </Navbar.Brand>
         <Navbar.Brand>
-          <img src={link} alt="" />
+          <img className={styles.logo} src={rodeo} alt="" />
         </Navbar.Brand>
         <Navbar.Brand>
-          <img src={link} alt="" />
+          <img className={styles.logo} src={russel} alt="" />
         </Navbar.Brand>
         <Navbar.Brand>
-          <img src={link} alt="" />
+          <img className={styles.logo} src={uncuyo} alt="" />
         </Navbar.Brand>
       </Navbar>
     </div>
--- frontend/pagina-amebal/src/Footer.js
+++ frontend/pagina-amebal/src/Footer.js
@@ -2,36 +2,41 @@ import React from 'react'
 import './App.css';
 import "bootstrap/dist/css/bootstrap.css";
 import { Navbar, Nav } from "react-bootstrap";
+import styles from "./Footer.module.css";
+
 import logocd from "./imgs/logo-amebal-cd.png";
+import ubicacion from "./imgs/ubicacion-logo.png";
+import instagram from "./imgs/instagram-logo.png";
+import facebook from "./imgs/facebook-logo.png";
+import twitter from "./imgs/twitter-logo.png";
 
 export default function Footer() {
-    const ubicacion = 'https://placedog.net/40/40';
-    const instagram = 'https://placedog.net/40/40';
-    const twitter = 'https://placedog.net/40/40';
-    const facebook = 'https://placedog.net/40/40';
   return (
     <div className='footer'>
         <Navbar bg="dark" variant="dark">
             <Navbar.Brand>
-                <img src={logocd} style={{ width:'5rem' }}/>
+                <img src={logocd} style={{ width:'3.5rem' }}/>
                 Asociación Mendocina de Balonmano
             </Navbar.Brand>
-            <Nav.Link href="ubicacion">
-                <img src={ubicacion} alt="ubicacion" />
-                San Juan 816, Capital, Mendoza
-            </Nav.Link>
-            <Nav.Link href="instagram">
-                <img src={instagram} alt="instagram" />
-                @Balonamebal
-            </Nav.Link>
-            <Nav.Link href="twitter">
-                <img src={twitter} alt="twitter" />
-                @Amebal0
-            </Nav.Link>
-            <Nav.Link href="facebook">
-                <img src={facebook} alt="facebook" />
-                Amebalmendoza
-            </Nav.Link>
+            <Nav className='right-aligned'>
+                <Nav.Link href="ubicacion">
+                    <img className={styles.icon} src={ubicacion} alt="ubicacion" />
+                    San Juan 816, Capital, Mendoza
+                </Nav.Link>
+                <Nav.Link href="instagram">
+                    <img className={styles.icon} src={instagram} alt="instagram" />
+                    @Balonamebal
+                </Nav.Link>
+                <Nav.Link href="twitter">
+                    <img className={styles.icon} src={twitter} alt="twitter" />
+                    @Amebal0
+                </Nav.Link>
+                <Nav.Link href="facebook">
+                    <img className={styles.icon} src={facebook} alt="facebook" />
+                    Amebalmendoza
+                </Nav.Link>
+            </Nav>
+            
         </Navbar>
     </div>
   )
