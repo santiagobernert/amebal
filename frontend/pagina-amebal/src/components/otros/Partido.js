6772847a94bec12e96558bf92d358f10d72efd58
santiagobernert <s.bernert@alumno.etec.um.edu.ar>
2022-04-29 14:57:44 -0300

    fixture añadido 29/4

--- frontend/pagina-amebal/src/components/otros/Partido.js
+++ frontend/pagina-amebal/src/components/otros/Partido.js
@@ -8,10 +8,18 @@ export default function ({ equipo1, equipo2 }) {
       <div className={styles.titulo}>
         <h4>{`${equipo1.nombre} vs ${equipo2.nombre}`}</h4>
       </div>
+        <div className={styles.detalles}>
+          <h6>Fecha 1</h6>
+          <h6>18 de Marzo 2022</h6>
+          <h6>Polideportivo JD Ribosqui</h6>
+        </div>
       <div className={styles.info}>
         <div className={styles.equipo}>
           <h4>{equipo1.abv}</h4>
           <img src={equipo1.imagen} alt={equipo1.abv} />
+        </div>
+        <div className={styles.resultado}>
+          <h4>38 - 37</h4>
         </div>
         <div className={styles.equipo}>
           <h4>{equipo2.abv}</h4>
--- frontend/pagina-amebal/src/components/paginas/Fixture.js
+++ frontend/pagina-amebal/src/components/paginas/Fixture.js
@@ -1,11 +1,110 @@
 import React from "react";
-//import styles from "../styles/Posiciones.module.css";
+
+import "bootstrap/dist/css/bootstrap.css";
+import { Col, Form, Row, InputGroup } from "react-bootstrap";
+
+import EQUIPOS from "../../equipos.js";
+
+import Calendario from "../otros/Calendario";
+import Partido from "../otros/Partido";
 import Titulo from "../otros/Titulo";
+import styles from "../../styles/Fixture.module.css";
 
 export default function Fixture() {
   return (
     <div>
       <Titulo text="FIXTURE" />
+      <div className={styles.main}>
+        <div className={styles.div_calendario}>
+          <Calendario />
+        </div>
+        <div className={styles.div_partidos}>
+          <Row>
+            <Col>
+            <Form.Label>Año</Form.Label>
+            <Form.Select aria-label="Default select example">
+            <option selected value="liga">
+              2022
+            </option>
+            <option value="1">2021</option>
+          </Form.Select>
+          </Col>
+            <Col>
+            <Form.Label>Fecha</Form.Label>
+            <Form.Select aria-label="Default select example">
+            <option selected value="liga">
+              Todos
+            </option>
+            <option value="1">Fecha 1</option>
+            <option value="1">Fecha 2</option>
+            <option value="2">Fecha 3</option>
+            <option value="3">Fecha 4</option>
+            <option value="3">Fecha 5</option>
+          </Form.Select>
+          </Col>
+            <Col>
+            <Form.Label>Club</Form.Label>
+            <Form.Select aria-label="Default select example">
+            <option selected value="liga">
+              Todos
+            </option>
+            {EQUIPOS &&
+            EQUIPOS.map((item) => (
+              <option
+                key={item.id}
+                value={item.nombre}
+              >{item.nombre}</option>
+            ))}
+          </Form.Select>
+            </Col>
+            <Col>
+            <Form.Label>Categoría</Form.Label>
+            <Form.Select aria-label="Default select example">
+            <option selected value="liga">
+              Todos
+            </option>
+            <option value="liga">Liga</option>
+              <option value="1">Mayores</option>
+              <option value="1">Junior</option>
+              <option value="2">Juvenil</option>
+              <option value="3">Cadetes</option>
+              <option value="3">Menores</option>
+              <option value="3">Infantiles</option>
+          </Form.Select>
+            </Col>
+            <Col>
+            <Form.Label style={{ color: "transparent" }}>cat</Form.Label>
+            <Form.Select aria-label="Default select example">
+              <option selected value="masculino">Masculino</option>
+              <option value="femenino">Femenino</option>
+            </Form.Select>
+            </Col>
+          </Row>
+          
+        
+          <h4>Partidos</h4>
+          <div className={styles.cont_partidos}>
+          <Partido
+            equipo1={EQUIPOS.find(
+              (x) => x.nombre === "Club Mendoza de Regatas"
+            )}
+            equipo2={EQUIPOS.find((x) => x.nombre === "Municipalidad de Maipu")}
+          />
+          <Partido
+            equipo1={EQUIPOS.find(
+              (x) => x.nombre === "Club Mendoza de Regatas"
+            )}
+            equipo2={EQUIPOS.find((x) => x.nombre === "Municipalidad de Maipu")}
+          />
+          <Partido
+            equipo1={EQUIPOS.find(
+              (x) => x.nombre === "Club Mendoza de Regatas"
+            )}
+            equipo2={EQUIPOS.find((x) => x.nombre === "Municipalidad de Maipu")}
+          />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
