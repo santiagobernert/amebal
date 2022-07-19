c22d35bac81efb71dd8dfc4a9e34fca4e9054742
santiagobernert <s.bernert@alumnno.etec.um.edu.ar>
2022-07-18 23:33:18 -0300

    terminado partidos y partido

--- frontend/pagina-amebal/src/components/otros/ClubCard.js
+++ frontend/pagina-amebal/src/components/otros/ClubCard.js
@@ -4,10 +4,10 @@ import { Card } from "react-bootstrap";
 export default function ClubCard({ img, titulo }) {
   return (
     <div>
-      <Card>
-        <a href={"clubes/" + titulo.replace(/ /g, "").toLowerCase()}>
-          <Card.Img src={img} alt={titulo} style={{ width: "15rem" }} />
-          <Card.Title>{titulo}</Card.Title>
+      <Card className="bg-transparent border-0 text-center mb-3">
+        <a className="text-dark" href={"clubes/" + titulo.replace(/ /g, "").toLowerCase()}>
+          <Card.Img className="max-vw-30" src={img} alt={titulo} style={{ width: "15rem" }} />
+          <Card.Title className="h6">{titulo}</Card.Title>
         </a>
       </Card>
     </div>
--- frontend/pagina-amebal/src/components/paginas/Club.js
+++ frontend/pagina-amebal/src/components/paginas/Club.js
@@ -1,91 +1,96 @@
 import React from "react";
 import { Card, Table } from "react-bootstrap";
 
+import styles from "../../styles/paginas/club/Club.module.css";
+
+import EQUIPOS from "../../lists/equipos.js";
+import PARTIDOS from "../../lists/partidos.js";
 import Titulo from "../otros/Titulo.js";
+import Partido from "../otros/Partido.js";
+import instagram from "../../imgs/instagram-logo.png";
+import facebook from "../../imgs/facebook-logo.png";
+import twitter from "../../imgs/twitter-logo.png";
 
 export default function Club({ nombre, imagen }) {
   return (
     <div>
       <Titulo text={nombre} />
+      <div className={styles.blob}></div>
       <div className="container">
         <div className="row">
+          <div className="col text-center">
+              <img src={imagen} alt={nombre} style={{ width: "calc(20vw + 20vh)" }} />
+          </div>
           <div className="col">
-            <Card>
-              <Card.Img src={imagen} alt={nombre} style={{ width: "20rem" }} />
-              <Card.Title>{nombre}</Card.Title>
-            </Card>
+            <div className="row p-2">
+              <div className="col pt-1">
+                <h5>Ubicación</h5>  
+                <iframe
+                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d53602.89746442965!2d-68.93297196875!3d-32.89337999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e09aef0d9b057%3A0xab741a983b2e45f1!2sClub%20Mendoza%20de%20Regatas!5e0!3m2!1ses-419!2sar!4v1658177176039!5m2!1ses-419!2sar"
+                 style={{border:"0", width:"70vw", height:"32vh"}} 
+                 allowfullscreen="" 
+                 loading="lazy" 
+                 referrerpolicy="no-referrer-when-downgrade"></iframe>
+                 <a href="/">Visitar sitio oficial</a>
+              </div>
+              <div className="col d-flex flex-column vw-10 pt-2 justify-content-center">
+                <a href="/"><img className={styles.logo} src={facebook} alt="" /></a>
+                <a href="/"><img className={styles.logo} src={twitter} alt="" /></a>
+                <a href="/"><img className={styles.logo} src={instagram} alt="" /></a>
+              </div>
+            </div>
           </div>
           <div className="col container">
-            <h2>Posiciones</h2>
-            <Table striped bordered hover>
+            <h3>Tabla de posiciones</h3>
+            <Table className={`${styles.tabla} mb-4 mt-3`} striped bordered hover>
               <thead>
                 <tr>
-                  <th>Categoría</th>
-                  <th>Posición</th>
-                  <th>Jugados</th>
-                  <th>Ganados</th>
-                  <th>Perdidos</th>
-                  <th>Empatados</th>
+                  <th>Cat.</th>
+                  <th>Pos</th>
+                  <th>J</th>
+                  <th>G</th>
+                  <th>P</th>
                   <th>DG</th>
-                  <th>Puntos</th>
+                  <th>PTS</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
+                  <td>Liga Masc</td>
                   <td>1</td>
-                  <td>[]</td>
-                  <td>Regatas</td>
                   <td>9</td>
                   <td>9</td>
                   <td>0</td>
                   <td>100</td>
-                  <td>10</td>
-                  <td>90</td>
                   <td>27</td>
                 </tr>
                 <tr>
-                  <td>2</td>
-                  <td>[]</td>
-                  <td>Regatas</td>
+                  <td>Juvenil Masc</td>
+                  <td>1</td>
                   <td>9</td>
                   <td>9</td>
                   <td>0</td>
                   <td>100</td>
-                  <td>10</td>
-                  <td>90</td>
                   <td>27</td>
                 </tr>
                 <tr>
-                  <td>3</td>
-                  <td>[]</td>
-                  <td>Regatas</td>
+                  <td>Cadetes Masc</td>
+                  <td>6</td>
                   <td>9</td>
                   <td>9</td>
                   <td>0</td>
                   <td>100</td>
-                  <td>10</td>
-                  <td>90</td>
                   <td>27</td>
                 </tr>
               </tbody>
             </Table>
-            <Table>
-              <thead>
-                <tr>
-                  <th>Categoría</th>
-                  <th>Nombre</th>
-                  <th>Goles</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>[]</td>
-                  <td>Regatas</td>
-                  <td>9</td>
-                </tr>
-              </tbody>
-            </Table>
+
+            <h3>Partidos</h3>
+            <div className={styles.cont_partidos}>
+              {PARTIDOS &&
+                PARTIDOS.map((item) => <Partido className={styles.partido} key={item.id} {...item} />)}
+            </div>
+
           </div>
         </div>
       </div>
