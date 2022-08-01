d1f9316e04416c06f2d6ffea596e322ef44d396e
santiagobernert <s.bernert@alumno.etec.um.edu.ar>
2022-08-01 10:59:26 -0300

    fixture desktop

--- frontend/pagina-amebal/src/components/paginas/Fixture.js
+++ frontend/pagina-amebal/src/components/paginas/Fixture.js
@@ -5,9 +5,11 @@ import { Col, Form, Row, InputGroup } from "react-bootstrap";
 
 import EQUIPOS from "../../lists/equipos.js";
 import PARTIDOS from "../../lists/partidos.js";
+import ARTICULOS from "../../lists/articulos.js";
 
 import Calendario from "../otros/Calendario";
 import Partido from "../otros/Partido";
+import Articulo from "../otros/Articulo";
 import Titulo from "../otros/Titulo";
 import styles from "../../styles/paginas/fixture/Fixture.module.css";
 
@@ -17,7 +19,6 @@ export default function Fixture() {
       <Titulo text="Fixture" />
       <div className={styles.blob}></div>
       <div className={styles.main}>
-        <div className={styles.div_partidos}>
           <Row className={styles.selects_row}>
             <Form.Select className={styles.select} readOnly aria-label="Default select example">
               <option value="todos">Fecha</option>
@@ -56,15 +57,20 @@ export default function Fixture() {
               <option value="femenino">Femenino</option>
             </Form.Select>
           </Row>
+        <div className={styles.div_partidos}>
 
-          <h4>Partidos</h4>
           <div className={styles.cont_partidos}>
             {PARTIDOS &&
               PARTIDOS.map((item) => <Partido className={styles.partido} key={item.id} {...item} />)}
+            <a href="/posiciones"><h5>Ver posiciones</h5></a>
           </div>
         </div>
         <div className={styles.div_calendario}>
           <Calendario />
+          {ARTICULOS &&
+                ARTICULOS.map((item) => (
+                  <Articulo key={item.id} titulo={item.titulo} img={item.img} />
+                ))}
         </div>
       </div>
     </div>
