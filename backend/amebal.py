22e6a52684c01eb0c5cb6f6c448dbf8c1efb91d0
santiagobernert <s.bernert@alumno.etec.um.edu.ar>
2022-08-12 14:33:10 -0300

    creado carpeta backend

--- backend/amebal.py
+++ backend/amebal.py
@@ -1,17 +1,11 @@
 from flask import Flask
-import jyserver.Flask as jsf
 
 app = Flask(__name__)
-
-@jsf.use(app)
-class App:
-    def __init__(self):
-        pass
 
 @app.route('/')
 @app.route('/home')
 def home():
-    pass
+    return {'result': 'hola perrito'}
 
 if __name__ == '__main__':
     app.run(debug=True)
