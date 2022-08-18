c27ae495d0c2e21b28f5b1f4032755a6a845e6cd
santiagobernert <s.bernert@alumno.etec.um.edu.ar>
2022-08-18 12:09:26 -0300

    archivos para tablas

---
+++ backend/db/clubes/clubes.py
@@ -0,0 +1,25 @@
+from . import db
+from mysql.connector import connect, Error
+
+
+create_clubes_table_query = """
+CREATE TABLE personas(
+    id INT PRIMARY KEY,
+    nombre VARCHAR(20),
+    apellido VARCHAR(20),
+    release_year YEAR(4),
+    genre VARCHAR(100),
+    collection_in_mil INT
+)
+"""
+try:
+    with connect(
+        host="localhost",
+        user=input("Enter username: "),
+        password=input("Enter password: "),
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(create_clubes_table_query)
+            connection.commit()
+except Error as e:
+    print(e)
---
+++ backend/db/db.py
@@ -0,0 +1,19 @@
+from mysql.connector import connect, Error
+
+try:
+    with connect(
+        host="localhost",
+        user=input("Enter username: "),
+        password=input("Enter password: "),
+    ) as connection:
+        create_db_query = "CREATE DATABASE sistema"
+        with connection.cursor() as cursor:
+            cursor.execute(create_db_query)
+except Error as e:
+    print(e)
+    
+show_db_query = "SHOW DATABASES"
+with connection.cursor() as cursor:
+    cursor.execute(show_db_query)
+    for db in cursor:
+        print(db)
