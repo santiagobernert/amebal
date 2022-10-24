79d389fb6d7ce659e6dffbe99ca67dc1147ada35
santiagobernert <s.bernert@alumno.etec.um.edu.ar>
2022-10-24 11:34:20 -0300

    crud tecnicos

--- backend/db/tecnicos/tecnicos.py
+++ backend/db/tecnicos/tecnicos.py
@@ -8,25 +8,28 @@ class Tecnico(db.Model):
     dni = db.Column(db.Integer, unique=True)
     nacimiento = db.Column(db.Date())
     sexo = db.Column(db.Enum('Masculino', 'Femenino'))
-    equipos = db.Column(db.Integer, db.ForeignKey('Equipos.id'))
+    asociacion = db.Column(db.Integer, db.ForeignKey('Asociaciones.id'))
+    nivel = db.Column(db.Integer)
+    equipos = db.Column(db.String(20))
     pases = db.relationship('Pase', backref='id_tecnico')
 
-    def __init__(self, id, nombre, apellido, dni, nacimiento, sexo, equipos, categoria):
+    def __init__(self, id, nombre, apellido, dni, nacimiento, sexo, asociacion, nivel, equipos):
         self.id = id
         self.nombre = nombre
         self.apellido = apellido
         self.dni = dni
         self.nacimiento = nacimiento
         self.sexo = sexo
+        self.asociacion = asociacion
+        self.nivel = nivel
         self.equipos = equipos
-        self.categoria = categoria
     
     def __asdict__(self):
-        return {'nombre':self.nombre, 'apellido':self.apellido, 'dni':self.dni, 'id':self.id, 'nacimiento':self.nacimiento, 'sexo':self.sexo, 'equipos':self.equipos, 'categoria':self.categoria}
+        return {'nombre':self.nombre, 'apellido':self.apellido, 'dni':self.dni, 'id':self.id, 'nacimiento':self.nacimiento, 'sexo':self.sexo, 'asociacion':self.asociacion, 'nivel':self.nivel, 'equipos':self.equipos}
 
 
-def nuevo_tecnico(id, nombre, apellido, dni, nacimiento, sexo, equipos, categoria):
-    tecnico = Tecnico(id, nombre, apellido, dni, nacimiento, sexo, equipos, categoria)
+def nuevo_tecnico(id, nombre, apellido, dni, nacimiento, sexo, asociacion, nivel, equipos):
+    tecnico = Tecnico(id, nombre, apellido, dni, nacimiento, sexo, asociacion, nivel, equipos)
     db.session.add(tecnico)
     db.session.commit()
     return tecnico
