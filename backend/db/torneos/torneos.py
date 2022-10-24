4fff27b558ef1a0c9117537fe9aaaf3a01451495
santiagobernert <s.bernert@alumno.etec.um.edu.ar>
2022-10-24 14:07:21 -0300

    torneos crud (falta probar)

--- backend/db/torneos/torneos.py
+++ backend/db/torneos/torneos.py
@@ -8,23 +8,25 @@ class Torneo(db.Model):
     fin = db.Column(db.Date)
     equipos = db.Column(db.String(100))
     ubicacion = db.Column(db.String(100))
+    categoria = db.Column(db.Integer, db.ForeignKey('Categorias.id'))
     partidos = db.relationship('Partido', backref='id_torneo')
     
 
-    def __init__(self, id, nombre, inicio, fin, equipos, ubicacion):
+    def __init__(self, id, nombre, inicio, fin, equipos, ubicacion, categoria):
         self.id = id
         self.nombre = nombre
         self.inicio = inicio
         self.fin = fin
         self.equipos = equipos
         self.ubicacion = ubicacion
+        self.categoria = categoria
 
     
     def __asdict__(self):
-        return  {'id':self.id, 'nombre':self.nombre, 'inicio':self.inicio, 'fin': self.fin, 'equipos': self.equipos, 'ubicacion': self.ubicacion}
+        return  {'id':self.id, 'nombre':self.nombre, 'inicio':self.inicio, 'fin': self.fin, 'equipos': self.equipos, 'ubicacion': self.ubicacion, 'categoria': self.categoria}
 
-def nuevo_torneo(id, nombre, inicio, fin, equipos, ubicacion):
-    torneo = Torneo(id, nombre, inicio, fin, equipos, ubicacion)
+def nuevo_torneo(id, nombre, inicio, fin, equipos, ubicacion, categoria):
+    torneo = Torneo(id, nombre, inicio, fin, equipos, ubicacion, categoria)
     db.session.add(torneo)
     db.session.commit()
     return torneo
