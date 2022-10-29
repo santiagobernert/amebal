98c653e2560720ae62855f6cf6716e60e846e8f9
santiagobernert <s.bernert@alumnno.etec.um.edu.ar>
2022-10-29 00:10:10 -0300

    eliminadas referencias en tabla de login

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,24 +2,11 @@ from flask import Flask
 from flask_login import LoginManager
 from os import path
 
-from db.usuarios.usuarios import Usuario
 
-from endpoints.Arbitros.arbitros import arbitros
 from endpoints.Articulos.articulos import articulos
-from endpoints.Asociaciones.asociaciones import asociaciones
-from endpoints.Clubes.clubes import clubes
-from endpoints.Categorias.categorias import categorias
-from endpoints.Equipos.equipos import equipos
-from endpoints.Imagenes.imagenes import imagenes
-from endpoints.Jugadores.jugadores import jugadores
 from endpoints.Login.login import login
-from endpoints.Mesas.mesas import mesas
 from endpoints.Pagos.pagos import pagos
 from endpoints.Partidos.partidos import partidos
-from endpoints.Pases.pases import pases
-from endpoints.Roles.roles import roles
-from endpoints.Sedes.sedes import sedes
-from endpoints.Tecnicos.tecnicos import tecnicos
 from endpoints.Torneos.torneos import torneos
 
 from db import db, DB_NAME, USERNAME, PASSWORD, create_database
@@ -32,22 +19,10 @@ def create_app():
     db.init_app(app)
 
 
-    app.register_blueprint(arbitros, url_prefix='/')
     app.register_blueprint(articulos, url_prefix='/')
-    app.register_blueprint(asociaciones, url_prefix='/')
-    app.register_blueprint(categorias, url_prefix='/')
-    app.register_blueprint(clubes, url_prefix='/')
-    app.register_blueprint(equipos, url_prefix='/')
-    app.register_blueprint(imagenes, url_prefix='/')
-    app.register_blueprint(jugadores, url_prefix='/')
     app.register_blueprint(login, url_prefix='/')
-    app.register_blueprint(mesas, url_prefix='/')
     app.register_blueprint(pagos, url_prefix='/')
     app.register_blueprint(partidos, url_prefix='/')
-    app.register_blueprint(pases, url_prefix='/')
-    app.register_blueprint(roles, url_prefix='/')
-    app.register_blueprint(sedes, url_prefix='/')
-    app.register_blueprint(tecnicos, url_prefix='/')
     app.register_blueprint(torneos, url_prefix='/')
 
     
--- backend/endpoints/Login/login.py
+++ backend/endpoints/Login/login.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, redirect, url_for, Blueprint, render_template, flash, jsonify
 from flask_login import login_user, login_required, logout_user, current_user
-from db.usuarios.usuarios import Usuario, nuevo_usuario
 from db import db
 from werkzeug.security import generate_password_hash, check_password_hash
+
 
 login = Blueprint('login', __name__)
 
@@ -14,8 +14,8 @@ def signup():
         contraseña = request.form.get('contraseña')
         contraseña2 = request.form.get('contraseña2')
 
-        us_existe = Usuario.query.filter_by(nombre=usuario).first()
-        email_existe = Usuario.query.filter_by(nombre=email).first()
+        us_existe = False #Usuario.query.filter_by(nombre=usuario).first()
+        email_existe = False #Usuario.query.filter_by(nombre=email).first()
         
         if us_existe or email_existe:
             print('usuario ya existe')
@@ -33,10 +33,10 @@ def signup():
         elif len(usuario) < 3:
             flash('nombre de usuario muy corto')
         else:
-            nu = nuevo_usuario(usuario, email, generate_password_hash(contraseña, method='sha256'))
+            #nu = nuevo_usuario(usuario, email, generate_password_hash(contraseña, method='sha256'))
             print('usuario creado')
             flash('usuario creado')
-            login_user(nu, remember=True)
+            #login_user(nu, remember=True)
             return redirect(url_for('login.log_in'))
 
     return render_template('index.html', pagina='signup', error='no se encontro usuario', usuario='')
@@ -47,7 +47,7 @@ def log_in():
         email = request.form.get('email')
         contraseña = request.form.get('contraseña')
 
-        usuario = Usuario.query.filter_by(email=email).first()
+        usuario = True #Usuario.query.filter_by(email=email).first()
         if usuario:
             if check_password_hash(usuario.contraseña, contraseña):
                 print('Logged in successfully!')
@@ -72,10 +72,10 @@ def logout():
 
 @login.route('/usuario', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def usuario():
-    usuarios = Usuario.query.all()
+    usuarios = [] #Usuario.query.all()
     print([u.__asdict__() for u in usuarios])
     if request.method == 'GET':
-        usuarios = Usuario.query.all()
+        usuarios = [] #Usuario.query.all()
         response = jsonify({
             'usuarios': [u.__asdict__() for u in usuarios],
             })
@@ -90,8 +90,8 @@ def usuario():
         contraseña = request.json['contraseña']
         rol = request.json['rol']
 
-        id_existe = Usuario.query.filter_by(id=id).first()
-        dni_existe = Usuario.query.filter_by(dni=dni).first()
+        id_existe = False #Usuario.query.filter_by(id=id).first()
+        dni_existe = False #Usuario.query.filter_by(dni=dni).first()
         
         if id_existe:
             print('id ya existe')
@@ -100,9 +100,9 @@ def usuario():
             print('usuario ya existe')
             return 'usuario ya existe'
         else:
-            nuevo_usuario(id, nombre, apellido, dni, email, contraseña, rol)
+            #nuevo_usuario(id, nombre, apellido, dni, email, contraseña, rol)
             print(f'Usuario {id} {nombre} {apellido} {dni} {rol}, creado')
-            usuarios = Usuario.query.all()
+            usuarios = [] #Usuario.query.all()
             response = jsonify({
             'usuarios': [u.__asdict__() for u in usuarios],
             })
@@ -114,7 +114,7 @@ def usuario():
         valores = request.get_json()
         id = valores['id']
         print(valores)
-        usuario = Usuario.query.filter_by(id=id).first()
+        usuario = {} #Usuario.query.filter_by(id=id).first()
         #clubes_relacionados = Club.query.filter_by(usuario=id)
         #clubes_relacionados.update({'usuario': None})
         print(usuario.nombre, usuario.dni, usuario.email)
@@ -127,7 +127,7 @@ def usuario():
         usuario.rol = valores['rol']
         db.session.commit()
         print('Usuario ', id, ' editado')
-        usuarios = Usuario.query.all()
+        usuarios = [] #Usuario.query.all()
         print([u.__asdict__() for u in usuarios])
         response = jsonify({
             'usuarios': [u.__asdict__() for u in usuarios],
@@ -139,11 +139,11 @@ def usuario():
         print('delete')
         id = request.get_json()
         print(id)
-        usuario = Usuario.query.filter_by(id=id)
+        usuario = {} #Usuario.query.filter_by(id=id)
         usuario.delete()
         db.session.commit()
         print('Usuario ', id, ' eliminado')
-        usuarios = Usuario.query.all()
+        usuarios = [] #Usuario.query.all()
         print([u.__asdict__() for u in usuarios])
         response = jsonify({
             'usuarios': [u.__asdict__() for u in usuarios],
